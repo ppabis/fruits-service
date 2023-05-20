3f7f9e85316ebb0257677d6048e94d379fab6461
ppabis <piotr@pabis.eu>
2023-05-20 18:03:09 +0200

    Different endpoint for fruits microservice accessed from JS

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"crypto/ecdsa"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"time"
@@ -14,7 +15,8 @@ var DbFile = "monolith.db"
 var RedisEndpoint = "localhost:6379"
 var TokenLifetime = 15 * time.Second
 var PrivateKey *ecdsa.PrivateKey
-var FruitsEndpoint = "http://localhost:8081"
+var FruitsEndpoint = "http://fruits:8081"
+var FruitsEndpointExternal = "http://localhost:8081"
 
 func init() {
 	var err error = nil
@@ -23,11 +25,13 @@ func init() {
 	if dbFile != "" {
 		DbFile = dbFile
 	}
+	log.Default().Printf("Using database file %q", DbFile)
 
 	redisEndpoint := os.Getenv("USE_REDIS_ENDPOINT")
 	if redisEndpoint != "" {
 		RedisEndpoint = redisEndpoint
 	}
+	log.Default().Printf("Using Redis endpoint %q", RedisEndpoint)
 
 	privateKey := []byte(os.Getenv("PRIVATE_KEY"))
 	if len(privateKey) == 0 {
@@ -35,6 +39,7 @@ func init() {
 		if privateKeyFile == "" {
 			privateKeyFile = "server.pem"
 		}
+		log.Default().Printf("Using private key file %q", privateKeyFile)
 		privateKey, err = os.ReadFile(privateKeyFile)
 		if err != nil {
 			panic(fmt.Errorf("could not read private key: %w", err))
@@ -54,10 +59,18 @@ func init() {
 			panic(fmt.Errorf("could not parse token lifetime: %w", err))
 		}
 	}
+	log.Default().Printf("Setting token lifetime to %v", TokenLifetime)
 
 	fruitsEndpoint := os.Getenv("FRUITS_ENDPOINT")
 	if fruitsEndpoint != "" {
 		FruitsEndpoint = fruitsEndpoint
 	}
+	log.Default().Printf("Using fruits endpoint %q", FruitsEndpoint)
+
+	fruitsEndpointExternal := os.Getenv("FRUITS_ENDPOINT_EXTERNAL")
+	if fruitsEndpointExternal != "" {
+		FruitsEndpointExternal = fruitsEndpointExternal
+	}
+	log.Default().Printf("External fruits endpoint set to %q", FruitsEndpointExternal)
 
 }
